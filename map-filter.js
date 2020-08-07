62872e9e6813dfa6ad5a0beb0bb3702acfa6a487
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-07 16:06:31 +0600

    object map filter

--- map-filter.js
+++ map-filter.js
@@ -22,3 +22,27 @@ const bigger =  numbers.filter(x => x>5 );
 
 // find function output a value 
 const isThere = numbers.find(x => x > 5);
+
+// ////////////////////////////////////////////
+
+const students = [
+    {id:21, name: 'Omar Sunny'},
+    {id:31, name: "maannaa"},
+    {id:41, name: 'Minul islam faruqi'},
+    {id:51, name: "Mohammad Mainul islam"}
+];
+console.log(students[1].name);
+
+const nameArray = [];
+for(let i = 0; i<students.length; i++){
+    const nameElement = students[i].name;
+    nameArray.push(nameElement);
+}
+console.log(nameArray);
+
+const name = students.map(student => student.name);
+const ids = students.map(student => student.id);
+
+const bigger2 = students.filter(student => student.id>40);
+const biggerOne = students.find(student => student.id>40);
+console.log(biggerOne);
